4b61a734b8a314da4f420a2ef7acfb396bb9afa1
aathif12 <aathifahamad4@gma.com>
2026-02-21 17:44:34 +0530

    Refactor StudentController to pass students data to the view and update route definition for form

--- Mock-app/app/Http/Controllers/StudentController.php
+++ Mock-app/app/Http/Controllers/StudentController.php
@@ -7,12 +7,13 @@ class StudentController extends Controller
 {
     public function viewForm()
     {
-        return view('form');
+        $students = _student::all();
+        return view('form', compact('students'));
     }
     public function addStudent(Request $request)
     {
         //dd($request->all());
         _student::create($request->all());
-        ;
+        return redirect()->route('form')->with('success', 'Student added successfully!');
     }
 }
--- Mock-app/routes/web.php
+++ Mock-app/routes/web.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-Route::get('/', [StudentController::class, 'viewForm']);
+Route::get('/', [StudentController::class, 'viewForm'])->name('form');
 Route::post('/add-student', [StudentController::class, 'addStudent']);
